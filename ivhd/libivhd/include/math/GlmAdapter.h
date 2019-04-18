df5ed0fc04a88f8630f6e860c2dc6011414c097b
Bartek Minch <barmin1993@gmail.com>
2019-04-18 22:40:06 +0200

    Add: Next part of new structure.

---
+++ ivhd/libivhd/include/math/GlmAdapter.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <iostream>
+#include <string>
+
+namespace ivhd::math
+{
+	typedef glm::vec2 float2;
+	typedef glm::vec3 float3;
+	typedef glm::vec4 float4;
+
+	typedef glm::mat2x2 matrix2x2;
+	typedef glm::mat3x3 matrix3x3;
+	typedef glm::mat4x4 matrix4x4;
+}
+
+inline std::ostream& operator<<(std::ostream& stream, const ivhd::math::float2& vector)
+{
+	return stream << "("
+		+ std::to_string(vector.x) + ", "
+		+ std::to_string(vector.y) + ")";
+}
+
+inline std::ostream& operator<<(std::ostream& stream, const ivhd::math::float3& vector)
+{
+	return stream << "("
+		+ std::to_string(vector.x) + ", "
+		+ std::to_string(vector.y) + ", "
+		+ std::to_string(vector.z) + ")";
+}
+
+inline std::ostream& operator<<(std::ostream& stream, const ivhd::math::float4& vector)
+{
+	return stream << "("
+		+ std::to_string(vector.x) + ", "
+		+ std::to_string(vector.y) + ", "
+		+ std::to_string(vector.z) + ", "
+		+ std::to_string(vector.w) + ")";
+}
--- ivhd/libivhd/source/facade/FacadeCasterMds.cpp
+++ ivhd/libivhd/source/facade/FacadeCasterMds.cpp
@@ -2,3 +2,36 @@
 /// \author Bartosz Minch <minch@agh.edu.pl>
 /// \date 18.04.2019
 ///
+
+#include "facade/FacadeCasterMds.h"
+
+namespace ivhd::cast
+{ 
+	FacadeCasterMDS::FacadeCasterMDS()
+	{
+	}
+
+	void FacadeCasterMDS::cast() 
+	{
+	}
+
+	void FacadeCasterMDS::initialize()
+	{
+	}
+
+	void FacadeCasterMDS::finalize()
+	{
+	}
+
+	void FacadeCasterMDS::resetSettings()
+	{
+	}
+
+	void FacadeCasterMDS::saveSettings(std::ostream& out)
+	{
+	}
+
+	void FacadeCasterMDS::loadSettings(std::istream& in)
+	{
+	}
+}
