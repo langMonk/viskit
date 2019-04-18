d26070118cb38253040d14e503db2395f9b22c9f
Bartek Minch <barmin1993@gmail.com>
2019-04-18 22:40:06 +0200

    Add: Next part of new structure.

--- ivhd/libivhd/include/facade/FacadeCasterMds.h
+++ ivhd/libivhd/include/facade/FacadeCasterMds.h
@@ -4,3 +4,28 @@
 ///
 
 #pragma once
+
+#include "ivhd/ICaster.h"
+
+namespace ivhd::cast
+{
+
+	class FacadeCasterMDS : public ICaster
+	{
+		// public construction and destruction methods
+	public:
+		FacadeCasterMDS();
+
+		// ICaster implementation
+	public:
+		void cast() override;
+		void initialize() override;
+		void finalize() override;
+		void resetSettings() override;
+		void saveSettings(std::ostream& out) override;
+		void loadSettings(std::istream& in) override;
+
+	private:
+
+	};
+}
---
+++ ivhd/libivhd/source/cast/CasterMds.cpp
@@ -0,0 +1,17 @@
+///
+/// \author Bartosz Minch <minch@agh.edu.pl>
+/// \date 18.04.2019
+///
+
+#include "cast/CasterMds.h"
+
+namespace ivhd::cast
+{
+	CasterMds::CasterMds()
+	{
+	}
+	
+	CasterMds::~CasterMds()
+	{
+	}
+}
