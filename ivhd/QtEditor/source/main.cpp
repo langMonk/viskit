5f8edc76c8ceec2c8b4e51427a76501bb7a989f7
Bartek Minch <barmin1993@gmail.com>
2019-04-18 21:06:44 +0200

    {editor} Add: Simple testing if public ivhd API works.

--- ivhd/QtEditor/source/main.cpp
+++ ivhd/QtEditor/source/main.cpp
@@ -1,8 +1,11 @@
 #include "QtEditor.h"
 #include <QtWidgets/QApplication>
 
+#include <ivhd/InteractiveVizualizationBuilder.h>
+
 int main(int argc, char *argv[])
 {
+	std::shared_ptr<ivhd::IInteractiveVizualization> ivhd = ivhd::createIVHD();
 	QApplication a(argc, argv);
 	QtEditor w;
 	w.show();
