d35a0b7c9b03c55d7b6ebb759282b6392c58de9b
Bartek Minch <barmin1993@gmail.com>
2019-04-28 16:18:44 +0200

    Changes in project structure. Renamed test project to libivhd_tests

---
+++ ivhd/libivhd_tests/ivhd/InteractiveVisualizationBuilderTest.cpp
@@ -0,0 +1,26 @@
+///
+/// \author Bartosz Minch <minch@agh.edu.pl>
+/// \date 27.04.2019
+///
+
+#include <gtest/gtest.h>
+#include <ivhd/InteractiveVizualizationBuilder.h>
+#include <ivhd/IInteractiveVizualization.h>
+
+TEST(InteractiveVisualizationBuilderTest, createIVHD) 
+{
+	std::shared_ptr<ivhd::IInteractiveVizualization> ivhd = ivhd::createIVHD();
+	EXPECT_NE(ivhd, nullptr);
+}
+
+TEST(InteractiveVisualizationBuilderTest, createTwoInstances)
+{
+	std::shared_ptr<ivhd::IInteractiveVizualization> ivhd1 = ivhd::createIVHD();
+	std::shared_ptr<ivhd::IInteractiveVizualization> ivhd2 = ivhd::createIVHD();
+
+	EXPECT_NE(ivhd1, nullptr);
+	EXPECT_NE(ivhd2, nullptr);
+
+	ivhd1 = nullptr;
+	ivhd2 = nullptr;
+}
