74c8b7392ea8f74ba8b01df1afab87e7176d2e6f
Bartek Minch <barmin1993@gmail.com>
2019-04-28 16:18:44 +0200

    Changes in project structure. Renamed test project to libivhd_tests

--- ivhd/libivhd/source/ivhd/InteractiveVizualizationBuilder.cpp
+++ ivhd/libivhd/source/ivhd/InteractiveVizualizationBuilder.cpp
@@ -1,9 +1,15 @@
+///
+/// \author Bartosz Minch <minch@agh.edu.pl>
+/// \date 28.04.2019
+///
+
 #include "ivhd/InteractiveVizualizationBuilder.h"
+#include "facade/FacadeInteractiveVizualization.h"
 
 namespace ivhd
 { 
 	std::shared_ptr<IInteractiveVizualization> createIVHD()
 	{
-		return std::shared_ptr<IInteractiveVizualization>();
+		return std::make_shared<facade::FacadeInteractiveVizualization>();
 	}
 }
