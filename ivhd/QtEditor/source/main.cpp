8d73f0ca0c4ef398104c25b11972b90feba88d7c
Bartek Minch <barmin1993@gmail.com>
2019-05-09 18:33:37 +0200

    {QtEditor} Minor change.

--- ivhd/QtEditor/source/main.cpp
+++ ivhd/QtEditor/source/main.cpp
@@ -2,10 +2,14 @@
 #include <QtWidgets/QApplication>
 
 #include <ivhd/InteractiveVizualizationBuilder.h>
+#include <ivhd/IResourceFactory.h>
 
 int main(int argc, char *argv[])
 {
 	std::shared_ptr<ivhd::IInteractiveVizualization> ivhd = ivhd::createIVHD();
+	auto parser = ivhd->resourceFactory().createParser(ivhd::ParserType::Csv);
+	ivhd->loadDataFile("D:/Datasets/mnist_7k_pca30.csv", *parser);
+
 	QApplication a(argc, argv);
 	QtEditor w;
 	w.show();
