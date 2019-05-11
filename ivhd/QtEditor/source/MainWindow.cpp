220def95e286b39203901cb120177e0ece0088a6
Bartek Minch <barmin1993@gmail.com>
2019-05-11 14:59:26 +0200

    {QtEditor} Changes in layout.

--- ivhd/QtEditor/source/MainWindow.cpp
+++ ivhd/QtEditor/source/MainWindow.cpp
@@ -1,7 +1,31 @@
+#include <QFileDialog>
+#include <QDesktopServices>
+
 #include "MainWindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+}
+
+void MainWindow::on_pushButton_Open_clicked()
+{
+	QString fileName = QFileDialog::getOpenFileName(this,
+		tr("Choose dataset"), "",
+		tr("CSV format(*.csv);;All Files (*)"));
+
+	if (fileName.isEmpty())
+	{
+		return;
+	}
+	else
+	{
+
+	}
+}
+
+void MainWindow::on_pushButton_Exit_clicked()
+{
+	close();
 }
