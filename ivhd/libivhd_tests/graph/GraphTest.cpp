5d3ee321de266472ee2d5a3032c350cc1b5327fa
Bartek Minch <barmin1993@gmail.com>
2020-02-16 18:55:33 +0100

    {ivhd/tests} Add possibility to save and restore graph to/from disk.

--- ivhd/libivhd_tests/graph/GraphTest.cpp
+++ ivhd/libivhd_tests/graph/GraphTest.cpp
@@ -16,6 +16,31 @@
 
 using namespace ivhd;
 
+void saveToPlainText(Graph& graph)
+{
+	std::ofstream m_file;
+	m_file.open("kNN_graph.txt");
+
+	const auto kNN_count = graph.neighborsCount();
+	
+	for (auto i = 0; i < kNN_count; i++)
+	{
+		const auto neighbors = graph.getNeighbors(i);
+		if (neighbors.type == NeighborsType::Near)
+		{
+			m_file << neighbors.i << "," << neighbors.j << "," << neighbors.r << "," << "Near" << std::endl;
+		}
+		else if (neighbors.type == NeighborsType::Far)
+		{
+			m_file << neighbors.i << "," << neighbors.j << "," << neighbors.r << "," << "Far" << std::endl;
+		}
+		else
+		{
+			m_file << neighbors.i << "," << neighbors.j << "," << neighbors.r << "," << "Random" << std::endl;
+		}
+	}
+}
+
 namespace libivhd_test
 {
 	TEST(Graph, Generation)
@@ -25,9 +50,9 @@ namespace libivhd_test
 		std::vector<Logs> logs{};
 		size_t count = 0;
 
-		auto handler = [&logs, &count](LogLevel level, std::string message)
+		auto handler = [&logs, &count](LogLevel level, const std::string& message)
 		{
-			logs.push_back(std::make_pair(level, message));
+			logs.emplace_back(level, message);
 			count++;
 		};
 
@@ -48,28 +73,46 @@ namespace libivhd_test
 		profiler.stop();
 		profiler.measurementMs();
 
-		std::ofstream m_file;
-		m_file.open("kNN_graph.txt");
+		auto graph = particleSystem.neighbourhoodGraph();
+		auto kNN_count = graph.neighborsCount();
+
+		EXPECT_EQ(kNN_count, 21000); //every point has 3 NN
+	}
+
+	TEST(Graph, SaveLoad)
+	{
+		using Logs = std::pair<LogLevel, std::string>;
+
+		std::vector<Logs> logs{};
+		size_t count = 0;
+
+		auto handler = [&logs, &count](LogLevel level, const std::string& message)
+		{
+			logs.emplace_back(level, message);
+			count++;
+		};
+
+		core::Core core{ handler };
+		parse::ParserCSV parser{ core.system() };
+		particles::ParticleSystem particleSystem{ core.system() };
+		generate::BruteForce generator{ core.system(), particleSystem };
+
+		auto csvFile = test_utils::resourcesDirectory().string() + "/mnist_7k_pca30.csv";
+		parser.loadFile(csvFile, particleSystem);
+
+		EXPECT_EQ(particleSystem.countParticles(), 7000);
+		EXPECT_EQ(particleSystem.originalCoordinates().size(), 7000);
+
+		generator.generate(3, 0, 0);
 
 		auto graph = particleSystem.neighbourhoodGraph();
-		size_t kNN_count = graph.neighborsCount();
-		for (int i = 0; i < kNN_count; i++)
-		{
-			auto neighbors = graph.getNeighbors(i);
-			if (neighbors.type == NeighborsType::Near)
-			{
-				m_file << neighbors.i << "," << neighbors.j << "," << neighbors.r << "," << "Near" << std::endl;
-			}
-			else if (neighbors.type == NeighborsType::Far)
-			{
-				m_file << neighbors.i << "," << neighbors.j << "," << neighbors.r << "," << "Far" << std::endl;
-			}
-			else
-			{
-				m_file << neighbors.i << "," << neighbors.j << "," << neighbors.r << "," << "Random" << std::endl;
-			}
-		}
+		graph.saveToCache("MNIST7k");
+		graph.clear();
+
+		EXPECT_EQ(graph.size(), 0);
+
+		graph.loadFromCache("MNIST7k");
 		
-		EXPECT_EQ(kNN_count, 21000); //every point has 3 NN
+		EXPECT_EQ(graph.size(), 21000);
 	}
 }
