691c52f14b5a5ffedc5173b01e48244f003475e4
Bartek Minch <barmin1993@gmail.com>
2020-02-21 20:55:58 +0100

    {ivhd/tests} kd-tree revisited. TODO: Adjust structure considering  interface.

--- ivhd/libivhd/include/ivhd/IGraph.h
+++ ivhd/libivhd/include/ivhd/IGraph.h
@@ -25,19 +25,17 @@ namespace ivhd
 		/// <summary>
 		/// Add one neighbor to graph.
 		/// </summary>
-		/// <param name="index"> Index of element to which add neighbor.</param>
 		/// <param name="neighbors"> Neighbor to add.</param>
-		void virtual addNeighbors(size_t index, Neighbors neighbor) = 0;
+		void virtual addNeighbors(Neighbors neighbor) = 0;
 
 		/// <summary>
 		/// Add vector of neighbors to graph.
 		/// </summary>
-		/// <param name="index"> Index of element to which add neighbors.</param>
 		/// <param name="neighbors"> Neighbors to add.</param>
-		void virtual addNeighbors(size_t index, std::vector<Neighbors> neighbors) = 0;
+		void virtual addNeighbors(std::vector<Neighbors> neighbors) = 0;
 
 		/// <summary>
-		/// Get neighbors count
+		/// Get overall neighbors count
 		/// </summary>
 		size_t virtual neighborsCount() = 0;
 
