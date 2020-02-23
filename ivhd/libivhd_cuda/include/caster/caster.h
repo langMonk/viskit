e234f1cba4eade15d4d3de7a52203d5246ad01c1
Bartek Minch <barmin1993@gmail.com>
2020-02-23 20:02:38 +0100

    {ivhd-cuda} Add cuda project and first chunk of code.

---
+++ ivhd/libivhd_cuda/include/caster/caster.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+#include <vector_types.h>
+#include <device_launch_parameters.h>
+#include "distance.h"
+#include "distance_container.h"
+
+using namespace std;
+
+class Caster : public IDistanceContainer
+{
+public:
+	Caster(int n, function<void(float)> onErrorCallback,
+		function<void(vector<float2>&)> onPositionsCallback)
+		: positions(n)
+	{
+		onError = onErrorCallback;
+		onPositions = onPositionsCallback;
+	};
+
+	virtual void simul_step() = 0;
+	virtual void prepare(vector<int>& labels) {};
+	virtual void finish() {};
+
+	vector<float2> positions;
+	function<void(float)> onError;
+	function<void(vector<float2>&)> onPositions;
+};
