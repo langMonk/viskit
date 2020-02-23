86865af5e31c72e85bddd2320b886ee615c1c547
Bartek Minch <barmin1993@gmail.com>
2020-02-23 23:33:41 +0100

    {cuda} Part1

--- ivhd/libivhd_cuda/include/CasterFactory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <memory>
-#include <vector>
-#include <ivhd\Structures.h>
-#include "caster/caster_cuda_ab.cuh"
-
-std::shared_ptr<Caster> getCaster(int n, std::function<void(float)> onError, std::function<void(std::vector<float2>&)> onPos, ivhd::CasterType type);
-
