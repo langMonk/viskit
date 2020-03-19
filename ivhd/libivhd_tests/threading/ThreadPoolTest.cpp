b0b947afa5d698b7d0b5f7b7b818313240e0212b
Bartek Minch <barmin1993@gmail.com>
2020-03-19 12:45:46 +0100

    {all} Fix: Change few depracated methods caused by the updated support for C++17. Retarget solution.

--- ivhd/libivhd_tests/threading/ThreadPoolTest.cpp
+++ ivhd/libivhd_tests/threading/ThreadPoolTest.cpp
@@ -16,11 +16,10 @@ namespace libivhd_test
 	
 	TEST(ThreadPool, DefaultThreadPool)
 	{
-		// Create pool with 3 threads
+		//// Create pool with 3 threads
 		ivhd::threading::ThreadPool pool(3);
 
-
-		// Submit (partial) multiplication table
+		//// Submit (partial) multiplication table
 		for (auto i = 1; i < 20; ++i) {
 			for (auto j = 1; j < 40; ++j) {
 				auto result = pool.enqueue(multiply, i, j);
