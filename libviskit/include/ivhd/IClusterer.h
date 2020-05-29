14dcf9d42285b55abfa527aae56873921b42fe08
Bartosz Minch <minch@agh.edu.pl>
2020-05-29 15:23:15 +0200

    {All} Rename libivhd->viskit + minor interface fixes.

---
+++ libviskit/include/ivhd/IClusterer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+///
+/// \author Bartosz Minch <minch@agh.edu.pl>
+/// \date 08.05.2019
+///
+
+#pragma once
+
+#include "ITransformer.h"
+
+namespace ivhd
+{
+	class IClusterer : public virtual ITransformer
+	{
+		// public methods 
+	public:
+
+
+	};
+}
