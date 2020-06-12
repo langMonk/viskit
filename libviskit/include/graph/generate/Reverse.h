e465496623aa2e676121388268f105bd64e978da
langMonk <lang.monk93@gmail.com>
2020-06-12 15:51:29 +0200

    {lib/editor} rKNN. part1

---
+++ libviskit/include/graph/generate/Reverse.h
@@ -0,0 +1,27 @@
+///
+/// \author Bartosz Minch <minch@agh.edu.pl>
+/// \date 12.06.2020
+///
+
+#pragma once
+
+#include "graph/generate/GraphGenerator.h"
+#include "particles/ParticleSystem.h"
+#include "graph/Graph.h"
+
+namespace ivhd::graph::generate
+{
+    class Reverse final : public GraphGenerator
+    {
+        // public construction and destruction methods
+    public:
+        explicit Reverse(core::System& system);
+
+        // public methods
+    public:
+        void generate(particles::ParticleSystem& ps, graph::Graph& graph, size_t k, bool distancesEqualOne) override;
+
+    private:
+        static bool validateIfReverseNeighbor(size_t index1, size_t index2);
+    };
+}
