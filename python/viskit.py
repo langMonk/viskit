85a2e1ba34ade549a63de41e1319878fd432fdda
langMonk <lang.monk93@gmail.com>
2021-11-13 16:14:40 +0100

    Fixed viskit.py for len(method) == 1

--- python/viskit.py
+++ python/viskit.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 from viskit.ivhd import IVHD
@@ -101,7 +100,8 @@ def main():
         plt.show()
         v = 0
 
-    metrics.visualize()
+    if len(methods) > 1:
+        metrics.visualize()
 
 
 if __name__ == "__main__":
