1e37c975f538474630b8f36ee3d22f198af070e9
langMonk <langMonk@proton.me>
2022-01-21 21:33:00 +0100

    Refactor whole viskit.py to save result per image (not combination). Add parameters to titles and naming conventions

--- python/viskit.py
+++ python/viskit.py
@@ -1,3 +1,5 @@
+import configparser
+import os
 import pandas as pd
 
 from viskit.embedding.ivhd import IVHD
@@ -6,17 +8,33 @@ from pylab import *
 from viskit.metrics.local_score import LocalMetrics
 
 
-def draw_2d(X, Y, labels, ax):
+def draw_2d(X, Y, labels):
     unique = list(set(labels))
     colors = [plt.cm.jet(float(i) / max(unique)) for i in unique]
 
     for i, u in enumerate(unique):
         xi = [X[j] for j in range(len(X)) if labels[j] == u]
         yi = [Y[j] for j in range(len(X)) if labels[j] == u]
-        if len(ax) < 1:
-            plt.scatter(xi, yi, color=colors[i], label=str(u), s=2)
-        else:
-            ax.scatter(xi, yi, color=colors[i], label=str(u), s=2)
+        plt.scatter(xi, yi, color=colors[i], label=str(u), s=2)
+
+
+def format_title(dataset_name: str, method) -> str:
+    if method["name"] != "IVHD":
+        generated_title = "{}_{}".format(dataset_name, method["name"])
+    else:
+        ivhd = method["object"]
+        generated_title = "{}_{}_opt={}_iter={}_nn={}_rn={}_l1steps={}_reverse_neighbors_steps={}_reverse_neighbors_count={}".format(
+            dataset_name,
+            method["name"],
+            ivhd.optimizer,
+            ivhd.n_iter,
+            ivhd.nn,
+            ivhd.rn,
+            ivhd.l1_steps,
+            ivhd.reverse_neighbors_steps,
+            ivhd.reverse_neighbors_count,
+        )
+    return generated_title
 
 
 def set_dataframe_columns(dataframe: pd.DataFrame) -> None:
@@ -29,6 +47,12 @@ def set_dataframe_columns(dataframe: pd.DataFrame) -> None:
 
 
 def main():
+    config = configparser.ConfigParser()
+    script_path = os.path.dirname(__file__)
+    config.read(script_path + "/config.ini")
+
+    logging.basicConfig(level=logging.INFO, filename=config["Paths"]["LogsPath"])
+
     dataset_files = [
         # "/home/bminch/Repositories/centroids/output/mnist_70k_pca30_50_2_all.csv",
         # "/Users/bartoszminch/Documents/Repositories/dataset_viskit/mnist.csv",
@@ -37,7 +61,35 @@ def main():
         # "/home/bminch/Repositories/dataset_viskit/mnist_7k.csv"
     ]
 
+    USE_METRICS = False
+
     methods = [
+        {
+            "name": "IVHD",
+            "object": IVHD(
+                optimizer="force-directed",
+                n_iter=1000,
+                nn=2,
+                rn=1,
+                l1_steps=0,
+                reverse_neighbors_steps=0,
+                reverse_neighbors_count=0,
+                graph_path="/Users/bartoszminch/Documents/Repositories/viskit/python/graphs/mnist_pca_100.bin",
+            ),
+        },
+        {
+            "name": "IVHD",
+            "object": IVHD(
+                optimizer="force-directed",
+                n_iter=1000,
+                nn=2,
+                rn=1,
+                l1_steps=0,
+                reverse_neighbors_steps=500,
+                reverse_neighbors_count=3,
+                graph_path="/Users/bartoszminch/Documents/Repositories/viskit/python/graphs/mnist_pca_100.bin",
+            ),
+        },
         {
             "name": "IVHD - force directed",
             "object": IVHD(
@@ -47,7 +99,46 @@ def main():
                 rn=1,
                 l1_steps=0,
                 reverse_neighbors_steps=500,
-                reverse_neighbors_count=500,
+                reverse_neighbors_count=4,
+                graph_path="/Users/bartoszminch/Documents/Repositories/viskit/python/graphs/mnist_pca_100.bin",
+            ),
+        },
+        {
+            "name": "IVHD - force directed",
+            "object": IVHD(
+                optimizer="force-directed",
+                n_iter=1000,
+                nn=2,
+                rn=1,
+                l1_steps=0,
+                reverse_neighbors_steps=500,
+                reverse_neighbors_count=6,
+                graph_path="/Users/bartoszminch/Documents/Repositories/viskit/python/graphs/mnist_pca_100.bin",
+            ),
+        },
+        {
+            "name": "IVHD - force directed",
+            "object": IVHD(
+                optimizer="force-directed",
+                n_iter=1000,
+                nn=2,
+                rn=1,
+                l1_steps=0,
+                reverse_neighbors_steps=500,
+                reverse_neighbors_count=8,
+                graph_path="/Users/bartoszminch/Documents/Repositories/viskit/python/graphs/mnist_pca_100.bin",
+            ),
+        },
+        {
+            "name": "IVHD - force directed",
+            "object": IVHD(
+                optimizer="force-directed",
+                n_iter=1000,
+                nn=2,
+                rn=1,
+                l1_steps=0,
+                reverse_neighbors_steps=500,
+                reverse_neighbors_count=10,
                 graph_path="/Users/bartoszminch/Documents/Repositories/viskit/python/graphs/mnist_pca_100.bin",
             ),
         },
@@ -56,53 +147,64 @@ def main():
         # {"name": "bh t-SNE", "object": TSNE(n_components=2, n_iter=2000)},
     ]
 
-    v = 0
-    metrics = LocalMetrics()
+    if not os.path.exists("results"):
+        os.makedirs("results")
+    os.chdir("results")
+
+    if USE_METRICS:
+        metrics = LocalMetrics()
+
     for dataset in dataset_files:
-        dataset_name = dataset.split("/")[-1]
+        dataset_name = dataset.split("/")[-1].strip(".csv")
+
+        if not os.path.exists(dataset_name):
+            os.makedirs(dataset_name)
+        os.chdir(dataset_name)
+
         X = pd.read_csv(dataset, delimiter=",", header=None, index_col=False)
         set_dataframe_columns(X)
 
         labels = X["label"]
         X = X.drop(labels=["label"], axis=1)
 
-        fig = plt.figure()
+        created = False
+        for method in methods:
+            if not created and not os.path.exists(method["name"]):
+                os.makedirs(method["name"])
+            if not created:
+                os.chdir(method["name"])
+            created = True
 
-        fig.set_figheight(16)
-        fig.set_figwidth(16)
-        ax = []
+            fig, ax = plt.subplots()
+            fig.set_figheight(16)
+            fig.set_figwidth(16)
+            fig.patch.set_visible(False)
+            ax.axis("off")
 
-        if len(methods) > 1:
-            for method in methods:
-                v = v + 1
-                ax = subplot(int(len(methods) / 2), 2, v)
-                ax.set_title("{} \n {}".format(dataset_name, method["name"]))
+            fig.suptitle(
+                format_title(dataset_name=dataset_name, method=method).replace(
+                    "_l1steps", "\nl1steps"
+                ),
+                fontsize=20,
+            )
 
-                # fit_transform of specific method
-                X_embedded = method["object"].fit_transform(X)
+            X_embedded = method["object"].fit_transform(X)
+            draw_2d(X_embedded[:, 0], X_embedded[:, 1], labels)
 
-                # draw 2d representation of the method
-                draw_2d(X_embedded[:, 0], X_embedded[:, 1], labels, ax)
+            fig.legend(labels=labels, markerscale=8.0, loc="right", fontsize=20)
 
-                # calculate metrics
+            if USE_METRICS:
                 metrics.calculate_knn_gain_and_dr_quality(
                     X_lds=X_embedded,
                     X_hds=X.values,
                     labels=labels.values,
                     method_name="{} {}".format(dataset_name, method["name"]),
                 )
-        else:
-            for method in methods:
-                X_embedded = method["object"].fit_transform(X)
-                draw_2d(X_embedded[:, 0], X_embedded[:, 1], labels, ax)
 
-        if len(ax) != 0:
-            handles, legend_labels = ax.get_legend_handles_labels()
-            fig.legend(labels=legend_labels, markerscale=8.0, loc="right", fontsize=20)
-        plt.show()
-        v = 0
+            plt.savefig(format_title(dataset_name=dataset_name, method=method))
+            plt.show()
 
-    if len(methods) > 1:
+    if USE_METRICS and len(methods) > 1:
         metrics.visualize()
 
 
