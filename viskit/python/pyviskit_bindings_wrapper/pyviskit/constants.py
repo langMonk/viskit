fd238ea6fbfd94f493ff09d715860c5d86456baa
Bartosz Minch <bartosz.minch@allmedica.pl>
2022-07-19 12:22:04 +0200

    black format

--- viskit/python/pyviskit_bindings_wrapper/pyviskit/constants.py
+++ viskit/python/pyviskit_bindings_wrapper/pyviskit/constants.py
@@ -1,17 +1,17 @@
-ITERATIONS = 'iterations'
-NEAREST_NEIGHBORS_COUNT = 'nearest_neighbors_count'
-RANDOM_NEIGHBORS_COUNT = 'random_neighbors_count'
-BINARY_DISTANCES = 'binary_distances'
-REVERSE_NEIGHBORS_STEPS = 'reverse_neighbors_steps'
-REVERSE_NEIGHBORS_COUNT = 'reverse_neighbors_count'
-L1_STEPS = 'l1_steps'
+ITERATIONS = "iterations"
+NEAREST_NEIGHBORS_COUNT = "nearest_neighbors_count"
+RANDOM_NEIGHBORS_COUNT = "random_neighbors_count"
+BINARY_DISTANCES = "binary_distances"
+REVERSE_NEIGHBORS_STEPS = "reverse_neighbors_steps"
+REVERSE_NEIGHBORS_COUNT = "reverse_neighbors_count"
+L1_STEPS = "l1_steps"
 
 DEFAULTS = {
-    'iterations': 2500,
-    'nearest_neighbors_count': 2,
-    'random_neighbors_count': 1,
-    'binary_distances': True,
-    'reverse_neighbors_steps': 0,
-    'reverse_neighbors_count': 0,
-    'l1_steps': 0
+    "iterations": 2500,
+    "nearest_neighbors_count": 2,
+    "random_neighbors_count": 1,
+    "binary_distances": True,
+    "reverse_neighbors_steps": 0,
+    "reverse_neighbors_count": 0,
+    "l1_steps": 0,
 }
--- viskit/python/pyviskit_bindings_wrapper/tryout.py
+++ viskit/python/pyviskit_bindings_wrapper/tryout.py
@@ -6,8 +6,8 @@ def visualize(df, type):
     classes = [i for i in range(10)]
 
     for cls in classes:
-        indices = df.loc[(df['class'] == cls)]
-        plt.scatter(indices['x'], indices['y'], label=f"{cls}", s=10)
+        indices = df.loc[(df["class"] == cls)]
+        plt.scatter(indices["x"], indices["y"], label=f"{cls}", s=10)
 
     plt.legend()
     print(f"Saving {type}")
@@ -16,21 +16,32 @@ def visualize(df, type):
 
 
 def invoke_fit_transform(caster, idx=None):
-    output_file = None if idx is None else f'visualization{idx}.csv'
+    output_file = None if idx is None else f"visualization{idx}.csv"
     return caster.fit_transform(
-        '../mnist_data.csv',
-        '../mnist_targets_2.csv',
-        '../mnist_euclidean.bin',
-        output_file
+        "../mnist_data.csv",
+        "../mnist_targets_2.csv",
+        "../mnist_euclidean.bin",
+        output_file,
     )
 
 
 visualize(invoke_fit_transform(Random.Random(types.OptimizerType.NONE)), "RANDOM_NONE")
-visualize(invoke_fit_transform(IVHD.IVHD(types.OptimizerType.Momentum)), "IVHD_MOMENTUM")
-visualize(invoke_fit_transform(IVHD.IVHD(types.OptimizerType.ForceDirected)), "IVHD_FORCEDIRECTED")
-visualize(invoke_fit_transform(IVHD.IVHD(types.OptimizerType.Adadelta)), "IVHD_Adadelta")
+visualize(
+    invoke_fit_transform(IVHD.IVHD(types.OptimizerType.Momentum)), "IVHD_MOMENTUM"
+)
+visualize(
+    invoke_fit_transform(IVHD.IVHD(types.OptimizerType.ForceDirected)),
+    "IVHD_FORCEDIRECTED",
+)
+visualize(
+    invoke_fit_transform(IVHD.IVHD(types.OptimizerType.Adadelta)), "IVHD_Adadelta"
+)
 visualize(invoke_fit_transform(IVHD.IVHD(types.OptimizerType.Adam)), "IVHD_Adam")
-visualize(invoke_fit_transform(IVHD.IVHD(types.OptimizerType.Nesterov)), "IVHD_Nesterov")
+visualize(
+    invoke_fit_transform(IVHD.IVHD(types.OptimizerType.Nesterov)), "IVHD_Nesterov"
+)
 visualize(invoke_fit_transform(IVHD.IVHD(types.OptimizerType.tSNE)), "IVHD_tSNE")
-visualize(invoke_fit_transform(LargeVis.LargeVis(types.OptimizerType.NONE)), "Largevis_None")
+visualize(
+    invoke_fit_transform(LargeVis.LargeVis(types.OptimizerType.NONE)), "Largevis_None"
+)
 print("done")
